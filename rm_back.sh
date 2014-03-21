7e5f3f3670d43d5f3a03d760ea08c2317f295174
localtest <jiazhaoteng@gmail.com>
2014-03-22 00:02:06 +0800

    Arrange the function And support the function of remove dictionary

--- rm_back.sh
+++ rm_back.sh
@@ -3,22 +3,32 @@
 #The path of the file to be deleted needs to be absolute path
 rm_back() {
 	#simple file
-	if [ -f $1 ]; then
-		du -k $1 > /tmp/rm_tmp
-		TEMP_DU=`awk '{print $1}' /tmp/rm_tmp`
-		echo $TEMP_DU
-		FIVE_M=5120
-		#less than 5M
-		if [ $TEMP_DU -lt $FIVE_M ]; then
-    			#make backup dir
-			if [ ! -d ~/rm_backdir ]; then
-    				mkdir ~/rm_backdir
-    			fi
-			#record rm log
-			echo $1	>> ~/rm_back.log
-			cp $1 ~/rm_backdir
+#if [ -f $1 ]; then
+		sourcefile=$1
+		if [ ${sourcefile:0:1} != "/" ]; then
+			filename=`pwd $1`
+			filename=${filename}"/"$1
+		else
+			filename=$sourcefile
 		fi
-	fi
-	rm $1
+		du -sk $filename > /tmp/rm_tmp
+		FILE_CAPACITY=`awk '{print $1}' /tmp/rm_tmp`
+		echo "The capacity of the file you removed is "$FILE_CAPACITY"k!"
+
+		#less than 99M
+		MAX_CAPACITY=101376
+		if [ $FILE_CAPACITY -lt $MAX_CAPACITY ]; then
+   			#check and make backup dir
+			if [ ! -d ~/rm_backdir ]; then
+   				mkdir ~/rm_backdir
+			else 
+				rm -fr ~/rm_backdir/*
+   			fi
+			#record rm log
+			echo $filename	> ~/rm_back.log
+			cp -R $filename ~/rm_backdir/
+		fi
+#fi
+rm -fr $1
 }
 rm_back $*
