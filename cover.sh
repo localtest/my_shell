f566fe75bd0d980dd24e954a13aecfb82a7af428
贾朝藤 <jiazhaoteng@jiachaotengdeMacBook-Pro.local>
2015-07-19 21:34:10 +0800

    1. support the remote url download & process 2. support the remote url download retry in case the the network jitter

--- cover.sh
+++ cover.sh
@@ -6,19 +6,40 @@ source /etc/profile
 ###
 ### Todo
 ###  1. compatible with the tool mediaInfo to detect image width x height
-###  2.
-###  3.
-
+###  2. add a option to chose delete the src image or not
 ####
 
 
+##
+# base config
+# Todo
+# 	1. add the necessary annotation here
+##
 SRC_IMG=$1
-DEST_IMG=$2
-WIDTH=$3
-HEIGHT=$4
-
+WIDTH=$2
+HEIGHT=$3
+DELETE_SRC_LOCAL_FILE=$4
 DEST_WIDTH=510
 DEST_HEIGHT=286
+
+SRC_IMG_PATH="/tmp"
+DEST_IMG_PATH="./trans"
+LOG_PATH="./debug.log"
+if [ ! -d "$DEST_IMG_PATH" ]; then 
+    mkdir "$DEST_IMG_PATH" 
+fi 
+
+FILE_NAME=`basename ${SRC_IMG}`
+DEST_IMG="${DEST_IMG_PATH}/${FILE_NAME}"
+
+wget -P $SRC_IMG_PATH -q $SRC_IMG
+#源视频下载失败重试
+for (( i=0; i<3; i++)); do
+    #detect the file name
+    if [ ! -f "$SRC_IMG_PATH/$FILE_NAME" ]; then 
+        wget -P $SRC_IMG_PATH -q $SRC_IMG
+    fi 
+done
 
 BACKGROUND=""
 THUMBNAIL=""
@@ -27,7 +48,7 @@ if [ $WIDTH -le $DEST_WIDTH -a $HEIGHT -le $DEST_HEIGHT ]; then
 	BACKGROUND="-background black"
 elif [ $WIDTH -gt $DEST_WIDTH -a $HEIGHT -gt $DEST_HEIGHT ]; then
         #if [ $WIDTH -le $HEIGHT ];then
-                THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}^"
+                THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}"
                 BACKGROUND="-background black"
         #else
         #        THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}^"
@@ -42,3 +63,12 @@ elif [ $HEIGHT -gt ${DEST_HEIGHT} ];then
 fi
 
 gm convert $SRC_IMG -thumbnail $THUMBNAIL $BACKGROUND -gravity center -extent ${DEST_WIDTH}x${DEST_HEIGHT} $DEST_IMG
+
+#DELETE SRC FILE
+#if [$DELETE_SRC_LOCAL_FILE -eq 1]
+	rm -f "$SRC_IMG_PATH/$FILE_NAME"
+#fi
+
+#写log
+REPLACE_TIME=$(date +%Y-%m-%d_%H:%M:%S)
+echo -e "$REPLACE_TIME src_img:$SRC_IMG src_width:$WIDTH src_height:$HEIGHT $THUMBNAIL $BACKGROUND" >> $LOG_PATH
