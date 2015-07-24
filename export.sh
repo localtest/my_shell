ebb7f459bc2c759c9f0e7ae8e5cae539545793d8
贾朝藤 <jiazhaoteng@jiachaotengdeMacBook-Pro.local>
2015-07-24 17:19:39 +0800

    add a script to export data from mysql direct

---
+++ export.sh
@@ -0,0 +1,13 @@
+###
+## Very Simple Script To Export Data From Mysql
+###
+
+SQL=$1
+DATA_FILE=$2
+
+HOST=''
+PORT=''
+USER=''
+PASSWD=''
+
+mysql -h $HOST -P $PORT -u $USER -p$PASSWD -Ne "${SQL}" > $DATA_FILE
