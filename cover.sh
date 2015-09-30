67187646c783fecf2dfeee684cf945a440a75575
贾朝藤 <jiazhaoteng@jiachaoengdeMBP.dhcp.yixia.com>
2015-09-30 15:53:11 +0800

    A Large Change Of The Cover.sh

--- cover.sh
+++ cover.sh
@@ -6,22 +6,12 @@ source /etc/profile
 ### very simple script to process image
 ###
 ### Todo
-###  2. compatible the suppot of remote(or local) video frame
-###  3. compatible local src image
-###  4. read from file queue to covert multi images
-###  5. add the surpot of regex src file name
-###  6. add some image 特效
-###  7. add the support of dir
-###  8. add the support to upload to cloud storage
-###  9.
-###  10.
-###  11.
-###  12.
+###  1. read from file queue to covert multi images
+###  2. add the surpot of regex src file name
+###  3. add the support of dir
+###  4. add the support to upload to cloud storage
+###  5.
 ####
-
-
-#ffmpeg -i $videoSource -y -f image2 -ss 00:01:00 -vframes 1 test1.jpg
-#ffmpeg -i $videoSource -y -f image2 -ss 60 -vframes 1 test1.jpg
 
 WIDTH=
 HEIGHT=
@@ -29,8 +19,12 @@ TYPE=
 EFFECT=
 DEST_WIDTH=
 DEST_HEIGHT=
+TARGET=
 DELETE_SRC_LOCAL_FILE=0
 SRC_IMG=
+OFFSET=0
+LEVEL=0
+COLOR=
 SRC_IMG_PATH="/tmp"
 DEST_IMG_PATH="/image_demo"
 if [ ! -d "$DEST_IMG_PATH" ]; then 
@@ -47,7 +41,7 @@ LOG_PATH="./debug.log"
 usage() {
 	echo
     printf >&1 "Usage:\n"
-    printf >&1 "Convert Image: ./cover.sh <-t convert> <-e effect> <-w width> <-h height> <-d delete> <-W width> <-H width> <-f address>\n"
+    printf >&1 "Convert Image: ./cover.sh <-t convert> <-e effect> <-w width> <-h height> <-d delete> <-W width> <-H width> <-o offset> <-m target name> <-l level> <-c color> <-f address>\n"
 	echo
     printf >&1 "       -t process type.\n"
     printf >&1 "       -e special effect.\n"
@@ -56,6 +50,10 @@ usage() {
     printf >&1 "       -d delete source file.\n"
     printf >&1 "       -W source image width.\n"
     printf >&1 "       -H source image height.\n"
+    printf >&1 "       -o offset.\n"
+    printf >&1 "       -m target name.\n"
+    printf >&1 "       -l process level.\n"
+    printf >&1 "       -c process color.\n"
     printf >&1 "       -f source image file.\n"
 
     echo
@@ -65,7 +63,7 @@ if [ -z "$1" ]; then
     exit 0
 fi
 
-while getopts "t:e:w:h:W:H:f:d" opt; do
+while getopts "t:e:w:h:W:H:o:m:l:c:f:d" opt; do
   case $opt in
     t) TYPE="$OPTARG";;
     e) EFFECT="$OPTARG";;
@@ -73,6 +71,10 @@ while getopts "t:e:w:h:W:H:f:d" opt; do
     h) HEIGHT="$OPTARG";;
     W) DEST_WIDTH="$OPTARG";;
     H) DEST_HEIGHT="$OPTARG";;
+    o) OFFSET="$OPTARG";;
+    m) TARGET="$OPTARG";;
+    l) LEVEL="$OPTARG";;
+    c) COLOR="$OPTARG";;
     f) SRC_IMG="$OPTARG";;
     d) DELETE_SRC_LOCAL_FILE=1;;
     \?)    usage;
@@ -86,60 +88,303 @@ if [ -z "$SRC_IMG" ]; then
 	exit 0
 fi
 
+####################
+### Download Remote File
+####################
+download() {
+	local src_img=$1
+	local file_name=$2
+	local retry_times=3
+
+	SRC_IMG="${SRC_IMG_PATH}/${file_name}"
+
+	local protocol=${src_img:0:8}
+	if [ $protocol != "http://" -a $protocol != "https://" ]; then
+		return
+	fi 
+	for (( i=0; i<$retry_times; i++)); do
+		if [ ! -f "$SRC_IMG_PATH/$file_name" ]; then 
+			wget -P $SRC_IMG_PATH -q $src_img -O $SRC_IMG
+		fi 
+	done
+}
+#Todo: One Detect Grep Twice
+detect_width_height() {
+	local src_img=$1
+	if [ ! $WIDTH ]; then
+		WIDTH=`mediainfo $src_img | grep 'Width' | awk '{print $3}'`
+	fi
+	if [ ! $HEIGHT ]; then
+		HEIGHT=`mediainfo $src_img | grep 'Height' | awk '{print $3}'`
+	fi
+}
+
 FILE_NAME=`basename ${SRC_IMG}`
+if [ "$TARGET" != "" ]; then
+	FILE_NAME=$TARGET
+fi
+if [ "$TYPE" != "snapshot" ]; then
+	download $SRC_IMG $FILE_NAME
+	detect_width_height $SRC_IMG
+fi
 DEST_IMG="${DEST_IMG_PATH}/${FILE_NAME}"
 
-#Todo
-# 1. check if it's the remote url
-# 2. if it's the local path, it will not delete local file by default
 
-wget -P $SRC_IMG_PATH -q $SRC_IMG
-#源视频下载失败重试
-for (( i=0; i<3; i++)); do
-    #detect the file name
-    if [ ! -f "$SRC_IMG_PATH/$FILE_NAME" ]; then 
-        wget -P $SRC_IMG_PATH -q $SRC_IMG
-    fi 
-done
-SRC_IMG="${SRC_IMG_PATH}/${FILE_NAME}"
+####################
+### Thumbnail Conf
+####################
+thumbnail_conf() {
+	local width=$1
+	local height=$2
+	local dest_width=$3
+	local dest_height=$4
 
-#Todo: one detect grep twice
-if [ ! $WIDTH ]; then
-	WIDTH=`mediainfo $SRC_IMG | grep 'Width' | awk '{print $3}'`
-fi
-if [ ! $HEIGHT ]; then
-	HEIGHT=`mediainfo $SRC_IMG | grep 'Height' | awk '{print $3}'`
-fi
+	local background=""
+	local thumbnail=""
+	local conf=""
+
+	if [ $width -le $dest_width -a $height -le $dest_height ]; then
+		thumbnail="${dest_width}x${dest_height}"
+		background="-background black"
+	elif [ $width -gt $dest_width -a $height -gt $dest_height ]; then
+			#if [ $width -le $height ];then
+					thumbnail="${dest_width}x${dest_height}"
+					background="-background black"
+			#else
+			#        thumbnail="${dest_width}x${dest_height}^"
+			#fi
+	elif [ $width -gt $dest_width ];then
+			#thumbnail="${dest_width}x${height}^"
+			thumbnail="${dest_width}x${height}"
+			background="-background black"
+	elif [ $height -gt ${dest_height} ];then
+			thumbnail="${dest_width}x${dest_height}"
+			background="-background black"
+	fi
+	conf="$thumbnail $background"
+	echo $conf
+}
+####################
+### Thumbnail
+####################
+do_thumbnail() {
+	local src_img=$1
+	local width=$2
+	local height=$3
+	local dest_width=$4
+	local dest_height=$5
+	local dest_img=$6
+
+	local conf=`thumbnail_conf $width $height $dest_width $dest_height`
+	gm convert $src_img -thumbnail $conf -gravity center -extent $dest_widthx$dest_height $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+
+	#Write Log
+	#REPLACE_TIME=$(date +%Y-%m-%d_%H:%M:%S)
+	#echo -e "$REPLACE_TIME src_img:$SRC_IMG src_width:$WIDTH src_height:$HEIGHT $THUMBNAIL $BACKGROUND" >> $LOG_PATH
+}
+
+####################
+### SnapShot
+####################
+do_snapshot() {
+	local src_video=$1
+	local dest_img=$2
+	local offset=$3
+	ffmpeg -i $src_video -y -f image2 -ss $offset -vframes 1 $dest_img
+}
+####################
+### SubTitles
+####################
+do_subtitles() {
+	echo "SubTitles"
+	#gm convert -font Arial -fill red -pointsize 18 -draw "text 200,100 '5'" test.jpg gif5.jpg
+	#gm convert -font ./simkai.ttf -fill red -pointsize 18 -draw "text 200,100 '烂柱子'" test.jpg test1.jpg
+}
+####################
+### LeftRight
+####################
+do_leftright() {
+	local src_img=$1
+	local dest_img=$2
+	gm convert -flop $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### UpDown
+####################
+do_updown() {
+	local src_img=$1
+	local dest_img=$2
+	gm convert -flip $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Scatter
+####################
+do_scatter() {
+	local src_img=$1
+	local dest_img=$2
+	#9
+	local degree=$3
+	gm convert -spread $degree $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Swirl
+####################
+do_swirl() {
+	local src_img=$1
+	local dest_img=$2
+	#67
+	local degree=$3
+	gm convert -swirl $degree $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Single Color
+####################
+do_single() {
+	local src_img=$1
+	local dest_img=$2
+	gm convert -monochrome $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Reverse Color
+####################
+do_reverse() {
+	local src_img=$1
+	local dest_img=$2
+	gm convert -negate $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Image Rotate
+####################
+do_rotate() {
+	local src_img=$1
+	local dest_img=$2
+	#+25
+	local degree=$3
+	gm mogrify -rotate +$degree $src_img
+	cp $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Image Border
+####################
+do_border() {
+	local src_img=$1
+	local dest_img=$2
+	#10x10
+	local degree=$3
+	local color=$4
+	gm mogrify -border $degree -bordercolor $color $src_img
+	cp $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
+####################
+### Image wave
+####################
+do_wave() {
+	local src_img=$1
+	local dest_img=$2
+	#100x1000
+	local degree=$3
+	gm mogrify -wave $degree $src_img
+	cp $src_img $dest_img
+
+	if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
+		   rm -f $src_img
+	fi
+}
 
 
-BACKGROUND=""
-THUMBNAIL=""
-if [ $WIDTH -le $DEST_WIDTH -a $HEIGHT -le $DEST_HEIGHT ]; then
-	THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}"
-	BACKGROUND="-background black"
-elif [ $WIDTH -gt $DEST_WIDTH -a $HEIGHT -gt $DEST_HEIGHT ]; then
-        #if [ $WIDTH -le $HEIGHT ];then
-                THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}"
-                BACKGROUND="-background black"
-        #else
-        #        THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}^"
-        #fi
-elif [ $WIDTH -gt $DEST_WIDTH ];then
-        #THUMBNAIL="${DEST_WIDTH}x${HEIGHT}^"
-        THUMBNAIL="${DEST_WIDTH}x${HEIGHT}"
-        BACKGROUND="-background black"
-elif [ $HEIGHT -gt ${DEST_HEIGHT} ];then
-        THUMBNAIL="${DEST_WIDTH}x${DEST_HEIGHT}"
-        BACKGROUND="-background black"
-fi
-
-gm convert $SRC_IMG -thumbnail $THUMBNAIL $BACKGROUND -gravity center -extent ${DEST_WIDTH}x${DEST_HEIGHT} $DEST_IMG
-
-#Delete source file
-if [ $DELETE_SRC_LOCAL_FILE -eq 1 ]; then
-       rm -f "$SRC_IMG_PATH/$FILE_NAME"
-fi
-
-#写log
-REPLACE_TIME=$(date +%Y-%m-%d_%H:%M:%S)
-echo -e "$REPLACE_TIME src_img:$SRC_IMG src_width:$WIDTH src_height:$HEIGHT $THUMBNAIL $BACKGROUND" >> $LOG_PATH
+support_list() {
+	echo
+    printf >&1 "Currently Support:\n"
+	echo
+    printf >&1 "       thumbnail	get a thumbnail from a image.\n"
+    printf >&1 "       snapshot		generage a snapshot of a video.\n"
+    printf >&1 "       subtitles	paste subtitles on the image.\n"
+    printf >&1 "       leftright	invert the image left and right.\n"
+    printf >&1 "       updown		invert the image up and down.\n"
+    printf >&1 "       scatter		scatter the image.\n"
+    printf >&1 "       swirl		swirl the image.\n"
+    printf >&1 "       single		get single color of the image.\n"
+    printf >&1 "       reverse		get reverse color of the image.\n"
+    printf >&1 "       rotate		rotate the image.\n"
+    printf >&1 "       border		border the image.\n"
+    printf >&1 "       wave		wave the image.\n"
+    echo
+}
+case $TYPE in
+    thumbnail)
+		do_thumbnail $SRC_IMG $WIDTH $HEIGHT $DEST_WIDTH $DEST_HEIGHT $DEST_IMG
+		;;
+    snapshot)
+		do_snapshot $SRC_IMG $DEST_IMG $OFFSET
+		;;
+    #subtitles)
+	#	do_subtitles
+	#	;;
+    leftright)
+		do_leftright $SRC_IMG $DEST_IMG
+		;;
+    updown)
+		do_updown $SRC_IMG $DEST_IMG
+		;;
+    scatter)
+		do_scatter $SRC_IMG $DEST_IMG $LEVEL
+		;;
+    swirl)
+		do_swirl $SRC_IMG $DEST_IMG $LEVEL
+		;;
+    single)
+		do_single $SRC_IMG $DEST_IMG
+		;;
+    reverse)
+		do_reverse $SRC_IMG $DEST_IMG
+		;;
+    rotate)
+		do_rotate $SRC_IMG $DEST_IMG $LEVEL
+		;;
+    border)
+		do_border $SRC_IMG $DEST_IMG $LEVEL $COLOR
+		;;
+    wave)
+		do_wave $SRC_IMG $DEST_IMG $LEVEL
+		;;
+    *)
+		support_list
+		exit 0
+		;;
+esac
